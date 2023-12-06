884efbcd89e3aef919b162377c21cc9cacf25865
Taslima Hussain Enas <101132793+enastaslima@users.noreply.github.com>
2023-12-06 09:18:08 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import './app.css'
+
+import Main from "./Components/Main/Main";
+ 
+
+
+const App = () => {
+    return (
+
+        <>
+        
+       
+       
+       
+        <Main />
+       
+
+        
+        </>
+    )
+}
+
+export default App
